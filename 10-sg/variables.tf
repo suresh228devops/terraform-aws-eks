d3d0c5425fbc8ac632bd2b80c14d2ab8b3062e1c
suresh228devops <suresh228devops@gmail.com>
2025-08-14 01:09:30 +0530

    Updated on 14082025

---
+++ 10-sg/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "bastion_sg_name" {
+    default = "bastion"
+}
+
+
+variable "bastion_sg_description" {
+    default = "created sg for bastion instance"
+}
+
---
+++ 60-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z10033673H6OAQSF2Q3OH"
+}
+
+variable "zone_name" {
+  default = "ksrdaws84s.site"
+}
